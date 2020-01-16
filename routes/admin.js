1f7361f90071c90f35dbdf514bc80c4e02df219b
Edson Vieira Demetrio <edsonvieirademetrio@gmail.com>
2020-01-15 22:54:09 -0200

    180. Admin Menu - List data DB

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require("express")
 var users = require('./../inc/users')
 var admin = require('./../inc/admin')
+var menus = require('./../inc/menus')
 var router = express.Router()
 
 router.get('/logout', function(req, res, next) {
@@ -86,7 +87,13 @@ router.get("/emails", function(req, res, next) {
 
 router.get("/menus", function(req, res, next) {
 
-    res.render("admin/menus", admin.getParams(req))
+    menus.getMenus().then(data => {
+
+        res.render("admin/menus", admin.getParams(req, {
+            data
+        }))
+    })
+
 })
 
 router.get("/reservations", function(req, res, next) {
